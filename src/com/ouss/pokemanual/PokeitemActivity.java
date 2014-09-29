5f91df1814a0c55b59ec6ed9bfee7105af4d5e05
bao050400287 <bao050400287@qq.com>
2014-09-29 14:02:36 +0800

    commoit

--- src/com/ouss/pokemanual/PokeitemActivity.java
+++ src/com/ouss/pokemanual/PokeitemActivity.java
@@ -1,5 +1,10 @@
 package com.ouss.pokemanual;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.ouss.pokemanual.common.DensityUtil;
 
 import android.app.ActionBar;
@@ -7,16 +12,21 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 public class PokeitemActivity extends Activity {
 
+	protected RequestQueue mRequestQueue;
 	private Context context;
+	private String pokeId;
+	private String pokeName;
+	private String url;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +37,9 @@ public class PokeitemActivity extends Activity {
 		Intent intent = this.getIntent();
 		Bundle bundle = intent.getExtras();
 		
-		String pokeId = bundle.getString("id");
-		String pokeName = bundle.getString("name");
-		String url = bundle.getString("url");
+		pokeId = bundle.getString("id");
+		pokeName = bundle.getString("name");
+		url = bundle.getString("url");
 		float dx = bundle.getFloat("dx");
 		float dy = bundle.getFloat("dy");
 		
@@ -39,6 +49,29 @@ public class PokeitemActivity extends Activity {
 		BitmapDrawable icon = (BitmapDrawable)PokeitemActivity.this.getResources().getDrawable(R.drawable.icon_0721);
 
 		actionBar.setIcon(drawableToBitmap(icon.getBitmap(), (int)Math.abs(dx), (int)Math.abs(dy)));
+		mRequestQueue = Volley.newRequestQueue(PokeitemActivity.this);
+		LoadData();
+	}
+	
+	
+	private void LoadData() {
+		StringRequest pokeInfoRequest = new StringRequest("http://wiki.52poke.com" + url, 
+	    		new Response.Listener<String>() {  
+		            @Override  
+		            public void onResponse(final String pokeInfo) {
+		            	ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBarPokeItem);
+		            	progressBar.setVisibility(View.GONE);
+		            	
+		            	LinearLayout pokeItemLayout = (LinearLayout)findViewById(R.id.pokeItemView);
+		            	pokeItemLayout.setVisibility(View.VISIBLE);
+		            }
+		}, new Response.ErrorListener() {  
+            @Override  
+            public void onErrorResponse(VolleyError error) {  
+                Log.e("TAG", error.getMessage(), error);  
+            }  
+        });
+		mRequestQueue.add(pokeInfoRequest);
 	}
 	
 	private Drawable drawableToBitmap(Bitmap drawable, int dx, int dy) {       
@@ -48,7 +81,7 @@ public class PokeitemActivity extends Activity {
         		DensityUtil.dip2px(context, 32),
         		DensityUtil.dip2px(context, 32));
         
-        bitmap = Bitmap.createScaledBitmap(bitmap, 128, 128, true);
+        bitmap = Bitmap.createScaledBitmap(bitmap, 256, 256, true);
 
         return new BitmapDrawable(bitmap);
 	}
---
+++ src/com/ouss/pokemanual/common/PokeHelper.java
@@ -0,0 +1,32 @@
+package com.ouss.pokemanual.common;
+
+import com.ouss.pokemanual.R;
+
+public class PokeHelper {
+	public enum PokeColor {  
+		normal("一般", R.color.normal, R.color.normal_border), 
+		fire("炎", R.color.fire, R.color.fire_border), 
+		fighting("格斗", R.color.fighting, R.color.fighting_border),
+		water("水", R.color.water, R.color.water_border),
+		flying("飞行", R.color.flying, R.color.flying_border),
+		grass("草", R.color.grass, R.color.grass_border),
+		poison("毒", R.color.poison, R.color.poison_border),
+		electric("电", R.color.electric, R.color.electric_border),
+		ground("地上", R.color.ground, R.color.ground_border),
+		psychic("超能力", R.color.psychic, R.color.psychic_border),
+		rock("岩石", R.color.rock, R.color.rock_border),
+		ice("冰", R.color.ice, R.color.ice_border),
+		bug("虫", R.color.bug, R.color.bug_border),
+		gragon("龙", R.color.gragon, R.color.gragon_border),
+		ghost("幽灵", R.color.ghost, R.color.ghost_border),
+		dark("恶", R.color.dark, R.color.dark_border),
+		steel("钢", R.color.steel, R.color.steel_border),
+		fairy("妖精", R.color.fairy, R.color.fairy_border);  
+	    // 成员变量  
+	    private String name;
+	    
+	    private PokeColor(String name, int bgColor, int borderColor) {  
+	        this.name = name;
+	    }  
+	}
+}
